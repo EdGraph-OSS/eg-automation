3c629fa56cbd64ab24343944bd216a3976112f8b
Roberto de León <json.roberto.deleon@outlook.com>
2026-06-22 13:16:17 -0600

    feat: remove unnecessary blacklist entries

--- ne_data_flow/_edfi_resources.py
+++ ne_data_flow/_edfi_resources.py
@@ -1,14 +1,6 @@
 from ed_fi.accessible_resources import BlacklistEntry, get_accessible_resources
 
-NDE_BLACKLIST: list[BlacklistEntry] = [
-    BlacklistEntry(module="edfixcrdc"),
-    BlacklistEntry(module="ed-fi-x-learning-modality"),
-    BlacklistEntry(module="ed-fi-xlearning-modality"),
-    BlacklistEntry(module="ed-fi-xassessment-roster"),
-    BlacklistEntry(resource="Descriptors"),
-    BlacklistEntry(resource="contacts"),
-    BlacklistEntry(resource="calendarDates"),
-]
+NDE_BLACKLIST: list[BlacklistEntry] = []
 
 
 async def get_entities_value(base_url: str, client_id: str, client_secret: str) -> str:
